9d936ed309462f7890d65a45fee958e647e06010
Arya Pramudya <aryapramudya.gunadarma@gmail.com>
2019-04-04 13:32:30 +0700

    Like and Dislike system for reply

--- app/Reply.php
+++ app/Reply.php
@@ -5,20 +5,22 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use App\Question;
 use App\User;
+use App\Like;
 
 class Reply extends Model
 {
 	protected $guarded = [];
-	
-    public function question(){
-    	return $this->belongsTo(Question::class);
+    
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
     }
-
-    public function user(){
-    	return $this->belongsTo(User::class);
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
-
-    public function like(){
-    	return $this->hasMany(Like::class);
+    public function like()
+    {
+        return $this->hasMany(Like::class);
     }
 }
