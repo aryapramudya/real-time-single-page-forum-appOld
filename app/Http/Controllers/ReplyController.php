410e1449c66b82d8b3db53f6d80ab83a34970c67
Arya Pramudya <aryapramudya.gunadarma@gmail.com>
2019-04-04 09:01:02 +0700

    CRUD for Reply

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Reply;
+use App\Question;
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Resources\ReplyResource;
 
 class ReplyController extends Controller
 {
@@ -11,9 +15,11 @@ class ReplyController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Question $question)
     {
-        //
+        //return $question->replies;
+        //return Reply::latest()->get();
+        return ReplyResource::collection($question->replies);
     }
 
     /**
@@ -22,9 +28,10 @@ class ReplyController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Question $question, Request $request)
     {
-        //
+        $reply = $question->replies()->create($request->all());
+        return response()->json(['reply'=>new ReplyResource($reply)],Response::HTTP_CREATED);
     }
 
     /**
@@ -33,9 +40,9 @@ class ReplyController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Question $question, Reply $reply)
     {
-        //
+        return new ReplyResource($reply);
     }
 
     /**
@@ -45,9 +52,13 @@ class ReplyController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Question $question, Request $request, Reply $reply)
     {
-        //
+        //$model = Question::find($request->id);
+        //$model->update();
+        $reply->update($request->all());
+        return response()->json('Updated',201);
+        //return $question;
     }
 
     /**
@@ -56,8 +67,10 @@ class ReplyController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Question $question, Reply $reply)
     {
-        //
-    }
+        $model = Reply::find($reply->id);
+        $model->delete();
+        return response()->json('Deleted',201);
+    }   
 }
